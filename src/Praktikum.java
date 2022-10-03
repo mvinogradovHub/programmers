9726c5c73dfa1116264b2b3a13f5171c0da25c4d
mvinogradovHub <114647156+mvinogradovHub@users.noreply.github.com>
2022-10-03 12:43:43 +0300

    убрал скобки

--- src/Praktikum.java
+++ src/Praktikum.java
@@ -18,10 +18,10 @@ public class Praktikum {
     public static boolean isLeapYear(int year) {
         // здесь нужно определить, является ли переданный год високосным
         boolean isLeapYear = false; // переменная истинна если висоскосный год
-        if ((year % 4) == 0) {
+        if (year % 4 == 0) {
             if ((year % 100) != 0) {
                 isLeapYear = true;
-            } else if ((year % 400) == 0) {
+            } else if (year % 400 == 0) {
                 isLeapYear = true;
             }
         }
